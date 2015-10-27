ddf56ed7eadcd7f129a7c532ab8683731f30cd36
JcBernack <jc.bernack@gmail.com>
2015-10-27 03:14:39 +0100

    Rework the layout list using the bootstrap thumbnail component

--- client/_rendering/layoutPreview.js
+++ client/_rendering/layoutPreview.js
@@ -10,19 +10,10 @@ Template.layoutPreview.helpers({
   viewBox: function () {
     var bb = this.boundingBox;
     return {
-      viewBox: [
-        bb.left - 1, " ",
-        bb.top - 1, " ",
-        bb.right - bb.left + 2, " ",
-        bb.bottom - bb.top + 2
-      ]
-    };
-  },
-  gridPosition: function () {
-    var bb = this.boundingBox;
-    return {
       x: bb.left - 1,
-      y: bb.top - 1
-    }
+      y: bb.top - 1,
+      width: bb.right - bb.left + 2,
+      height: bb.bottom - bb.top + 2
+    };
   }
 });
--- client/layouts/list.js
+++ client/layouts/list.js
@@ -7,10 +7,3 @@ Template.layoutList.helpers({
     return Layouts.find({}, { sort: { createdAt: -1 } });
   }
 });
-
-Template.layoutList.events({
-  "click button.remove": function (event) {
-    event.preventDefault();
-    Meteor.call("layout.remove", this._id);
-  }
-});
