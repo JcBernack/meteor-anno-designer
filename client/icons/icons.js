c231c34b1cafc089b8a7c2b9b141efd4eec2bf07
JcBernack <jc.bernack@gmail.com>
2015-11-03 03:35:27 +0100

    Add displayName to Icons

--- client/icons/icons.js
+++ client/icons/icons.js
@@ -1,10 +1,17 @@
 Template.iconList.helpers({
   icons: function () {
     return Icons.find();
+  },
+  active: function () {
+    return Session.equals("icons.selected", this._id) ? "active" : "";
   }
 });
 
 Template.iconList.events({
+  "click button.icon": function () {
+    Session.set("icons.selected", this._id);
+    console.log("selected icon: " + this._id);
+  },
   "tag.added": function (event) {
     Meteor.call("icon.tag.add", this._id, event.newTag);
   },
@@ -13,7 +20,83 @@ Template.iconList.events({
   }
 });
 
+Template.iconEditor.helpers({
+  selectedIcon: function () {
+    return Icons.findOne(Session.get("icons.selected"));
+  },
+  saveDisabled: function () {
+    var icon = Icons.findOne(Session.get("icons.selected"));
+    return icon ? "" : "disabled";
+  },
+  iconSchema: function () {
+    return new SimpleSchema({
+      displayName: {
+        type: String
+      }
+    });
+  }
+});
+
+AutoForm.hooks({
+  iconForm: {
+    onSubmit: function() {
+      try {
+        Icons.update(this.docId, this.updateDoc);
+        console.log("updated icon: " + this.docId);
+        // tell autoform that were done here
+        this.done();
+      } catch (err) {
+        // catch any error and pass it to the callback,
+        // otherwise a post back would occur because the "return false" was skipped
+        this.done(err);
+      }
+      // prevent post back of form submit
+      return false;
+    },
+    onError: function (formType, err) {
+      console.log(err);
+    }
+  }
+});
+
+Template.iconUpload.onCreated(function () {
+  this.tags = new ReactiveVar(["untagged"]);
+});
+
 Template.iconUpload.helpers({
+  tags: function () {
+    return Template.instance().tags.get();
+  }
+});
+
+Template.iconUpload.events({
+  "tag.added": function (event, template) {
+    var tags = template.tags.get();
+    tags.push(event.newTag);
+    template.tags.set(tags);
+  },
+  "tag.removed": function (event, template) {
+    var tags = template.tags.get();
+    var i = tags.indexOf(event.removedTag);
+    if (i > -1) tags.splice(i, 1);
+    template.tags.set(tags);
+  },
+  "change .input-icon-files": function (event, template) {
+    FS.Utility.eachFile(event, function (file) {
+      var newFile = new FS.File(file);
+      //TODO: validate on the server
+      newFile.displayName = newFile.name();
+      newFile.tags = template.tags.get();
+      Icons.insert(newFile, function (err, fileObj) {
+        // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+        console.log("uploading file: " + fileObj._id);
+      });
+    });
+    event.target.form.reset();
+  }
+});
+
+Template.iconUploadStatus.helpers({
   uploads: function () {
     var uploading = [];
     Icons.find().forEach(function (icon) {
@@ -22,20 +105,5 @@ Template.iconUpload.helpers({
       }
     });
     return uploading;
-  }
-});
-
-Template.iconUpload.events({
-  "change .input-file": function (event) {
-    FS.Utility.eachFile(event, function (file) {
-      var newFile = new FS.File(file);
-      //TODO: move to server
-      newFile.tags = ["untagged"];
-      Icons.insert(newFile, function (err, fileObj) {
-        // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
-        console.log("uploading file: " + fileObj._id);
-      });
-    });
-    event.target.form.reset();
   }
 });
