1879e3d9e80d810c96dd9358d3e614bd3233344f
JcBernack <jc.bernack@gmail.com>
2015-11-03 20:42:00 +0100

    Add preset picker to the designer Improve designer toolbar Rename layout object icon to iconId

--- client/designer/designer.js
+++ client/designer/designer.js
@@ -1,21 +1,68 @@
 Template.buildingSettings.onCreated(function () {
   // set initial building options
-  var defaultBuilding = { width: 4, height: 4, color: "#ff0000" };
-  var icon = Icons.findOne({ tags: { $all: ["warehouse"] } });
-  if (icon) defaultBuilding.icon = icon._id;
-  Session.setDefault("designer.placementObject", defaultBuilding);
+  var defaultPreset = Presets.findOne({ name: "Depot" });
+  Session.setDefault("designer.placementObject", {
+    color: "#ff0000",
+    label: "",
+    width: defaultPreset.width,
+    height: defaultPreset.height,
+    iconId: defaultPreset.iconId,
+    presetId: defaultPreset._id
+  });
   Session.setDefault("designer.placementActive", false);
 });
 
 Template.buildingSettings.helpers({
-  placementObject: function () {
-    return Session.get("designer.placementObject");
+  get: function (property) {
+    if (!property) return Session.get("designer.placementObject");
+    return Session.get("designer.placementObject")[property];
   },
-  buildingSchema: function () {
-    return LayoutObjectSchema.pick(["width", "height", "label", "color", "icon"]);
+  presetFilter: function () {
+    return { tags: "$all", name: "$in" };
   },
-  filterSelector: function () {
+  iconFilter: function () {
     return { tags: "$all", displayName: "$in" };
+  }
+});
+
+Template.buildingSettings.events({
+  "filtered-picker.change": function (event, template, data) {
+    console.log("picker changed");
+    var obj = Session.get("designer.placementObject");
+    obj[data.name] = data.selectedId;
+    Session.set("designer.placementObject", obj);
+  },
+  "submit": function (event, template) {
+    try {
+      var obj = {
+        color: event.target.color.value,
+        label: event.target.label.value
+      };
+      var preset = Presets.findOne(event.target.presetId.value);
+      if (preset) {
+        obj.width = preset.width;
+        obj.height = preset.height;
+        obj.iconId = preset.iconId;
+        obj.presetId = preset._id;
+      } else {
+        obj.width = parseInt(event.target.width.value);
+        obj.height = parseInt(event.target.height.value);
+        obj.iconId = event.target.iconId.value;
+      }
+      if (!Match.test(obj, LayoutObjectSchema.pick("color", "label", "width", "height", "iconId", "presetId"))) {
+        console.log("object invalid");
+      } else {
+        Session.set("designer.placementObject", obj);
+        Session.set("designer.placementActive", true);
+        console.log("placement started");
+      }
+    } catch (err) {
+      // catch any error and pass it to the callback,
+      // otherwise a post back would occur because the "return false" was skipped
+      console.log(err);
+    }
+    // prevent post back
+    return false;
   }
 });
 
@@ -36,21 +83,5 @@ Template.layoutDetails.events({
   },
   "tag.remove": function (event) {
     Meteor.call("layout.tag.remove", this._id, event.removedTag);
-  }
-});
-
-AutoForm.hooks({
-  buildingForm: {
-    onSubmit: function () {
-      Session.set("designer.placementObject", this.insertDoc);
-      Session.set("designer.placementActive", true);
-      console.log("placement started");
-      // prevent post back
-      this.done();
-      return false;
-    },
-    onError: function (formType, err) {
-      console.log(err);
-    }
   }
 });
